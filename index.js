806923adc6a913de97f66ae75b5c3f65cbe6deba
Dilshan-Kavishaka-Karunarathna <dilshankavishka667@gmail.com>
2024-07-30 15:30:46 +0530

    Code alignment arranging. add to %, /, (, ), operators.
    Arranging CSS, HTML, and JavaScript code alignment one by one. and add another operators.

--- index.js
+++ index.js
@@ -1,27 +1,19 @@
-
+//Calculator program
 
 const display = document.getElementById("display");
-
 function appendToDisplay(input){
-
     display.value += input;
-
 }
 
 function calculate(){
   try{
     display.value = eval(display.value);
   }
-
   catch(error){
     display.value = "ERROR";
   }
-
 }
 
 function allClear(){
- 
     display.value = "";
-
-
 }
